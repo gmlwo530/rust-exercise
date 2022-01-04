87b2ba1d530ebee1ab6f08d08c32286f7d47c95c
CHOI HEE JAE <gmlwo530@gmail.com>
2022-01-04 16:05:51 +0000

    first commit

---
+++ TheBook/projects/hello_world/main.rs
@@ -0,0 +1,9 @@
+// main 이름을 가지는 함수는 rust program이 실행 되면 제일 첫 번째로 실행되는 코드다.
+fn main() {
+    /*
+    - Rust의 indent tab이 아닌 스페이스를 사용한다
+    - !은 붙히면 Rust macro를 호출한다. 만약 !을 제거하면 일반 함수를 호출한다.
+    - semicolon을 붙혀주어야 한다.
+    */
+    println!("Hello, world!");
+}
