a55fead716c0c15f5407ba351c0640d9bba87d38
CHOI HEE JAE <gmlwo530@gmail.com>
2022-11-12 06:55:01 +0000

    Add BoomLabsRust

---
+++ BoomLabsRust/rust-basic/src/main_v200.rs
@@ -0,0 +1,100 @@
+#[derive(Clone)]
+struct Product {
+    name: String,
+    price: u64,
+    quantity: u64,
+    production_date: u64,
+}
+
+#[derive(Clone)]
+struct Employee {
+    name: String,
+    id: u64,
+}
+
+trait PrintInfo {
+    fn print_info(&self);
+}
+
+impl PrintInfo for Employee {
+    fn print_info(&self) {
+        println!(
+            "employee's name: {}\nemployee's id: {}\n",
+            self.name(),
+            self.id()
+        );
+    }
+}
+
+impl PrintInfo for Product {
+    fn print_info(&self) {
+        println!(
+            "Product's name: {}\nProduct's price: {}\nProduct quantity: {}\nProduction date: {}",
+            self.name,
+            self.price,
+            self.quantity,
+            self.date()
+        );
+    }
+}
+
+impl Product {
+    fn new(name: String, price: u64, quantity: u64, production_date: u64) -> Product {
+        Product {
+            name,
+            price,
+            quantity,
+            production_date,
+        }
+    }
+
+    fn date(&self) -> String {
+        let mut date = self.production_date;
+        let year: u64 = date / 10000;
+        date = date - year * 10000;
+        let month = date / 100;
+        date = date - month * 100;
+        format!("{}/{}/{}", year, month, date)
+    }
+}
+
+impl Employee {
+    fn new_from_default() -> Employee {
+        Employee {
+            name: "default".to_string(),
+            id: 100,
+        }
+    }
+
+    fn new(name: String, id: u64) -> Employee {
+        Employee { name, id }
+    }
+
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn id(&self) -> u64 {
+        self.id
+    }
+}
+
+// complie time에 어떤 데이터가 들어오는지 하드 코딩 됨
+fn static_print(data: impl PrintInfo) {
+    data.print_info();
+}
+
+// 런타임에 어떤 데이터가 들어오는 알 수 있음
+fn dynamic_print(data: Box<dyn PrintInfo>) {
+    data.print_info();
+}
+
+fn main() {
+    let employee = Employee::new("Jane".to_string(), 100);
+    let product = Product::new("Apple".to_string(), 1, 100, 20220924);
+
+    static_print(employee.clone());
+    static_print(product.clone());
+    dynamic_print(Box::new(employee));
+    dynamic_print(Box::new(product));
+}
